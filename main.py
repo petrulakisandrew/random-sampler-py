466db937ecfe5da36b6a2b59573ce330b5e6f382
Andrew Petrulakis <petrulakisandrew@gmail.com>
2025-02-11 16:26:18 -0600

    Importing random, datetime, and universals for testing

--- main.py
+++ main.py
@@ -2,4 +2,11 @@
 
 import os
 import pandas as pd 
+import random
+from datetime import datetime, timedelta
+
+
+CASEWORKERS = ['Candice', 'Morgan', 'Ozury', 'Nida', 'Scott']
+DATE_RANGE = ('01-01-2024', '12-31-2024')
+NUM_SAMPLES = 2
 
