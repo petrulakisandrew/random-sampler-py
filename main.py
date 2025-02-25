9501bb3e711342bf3ffebfcafb98b6b379ac3362
Andrew Petrulakis <petrulakisandrew@gmail.com>
2025-02-25 13:41:15 -0600

    Removed undesired code, Revised definition to run code for sampling procedures, tested with print statement

--- main.py
+++ main.py
@@ -8,42 +8,27 @@ from datetime import datetime, timedelta
 #Setting Excel File Directory
 REPORT_DIRECTORY = "C:/Users/Andrew Petrulakis/Desktop/Reports/SEMAP/SEMAP 3 AND 10/DHA/2025/Jan/Jan_Activity.xlsx"
 
-# CASEWORKERS = ['Candice', 'Morgan', 'Ozury', 'Nida', 'Scott']
-# DATE_RANGE = ('01-01-2024', '12-31-2024')
-# NUM_SAMPLES = 2
-
-def random_date(start_date: str, end_date: str, date_format: str = "%m-%d-%Y") -> str:
-    """ Generate a random date between two given dates.
-        - start_date: The start date of the range.
-        - end_date: The end date of the range.
-        - date_format: The format of the input and output dates.
+def random_sample(data: str, column: str, sample_size: int, sampled_data: str) -> str:
+    """ Generate a random sample based on excel column criteria.
+        - data: active dataframe being used for the sample
+        - column: the desired column in excel to be used for sample critera.
+        - sample_size: number of samples desired per object.
+        - sampled_data: name of newly created dataframe
     """
-    try:
-        start = datetime.strptime(start_date, date_format)
-        end = datetime.strptime(end_date, date_format)
-        
-    except ValueError as e:
-        raise ValueError(f"Incorrect date format, should be {date_format}") from e
-
-    if start > end:
-        raise ValueError("start_date must be earlier than end_date")
-
-    random_date = start + timedelta(days=random.randint(0, (end - start).days))
-    return random_date.strftime(date_format)
+    sampled_data = data.groupby(column).apply(lambda x: x.sample(n=min(len(x), sample_size),)).reset_index(drop=True)
+    print(sampled_data)
 
 #Reading in Excel File 
-data = pd.read_excel(REPORT_DIRECTORY)
-data.fillna('N/A', inplace=True)
+data_test = pd.read_excel(REPORT_DIRECTORY)
+data_test.fillna('N/A', inplace=True)
 
-#Isolatiing Column that will be used for sampling 
-caseworkers = data["Caseworker58"]
+# #Isolatiing Column that will be used for sampling 
+# caseworkers = data_test["Caseworker58"]
 
-#Random Sampling Based On Caseworker (2 for each person):
-sampled_data = data.groupby("Caseworker58").apply(lambda x: x.sample(n=min(len(x), 2),)).reset_index(drop=True)
+# #Random Sampling Based On Caseworker (2 for each person):
+# sampled_data = data_test.groupby("Caseworker58").apply(lambda x: x.sample(n=min(len(x), 2),)).reset_index(drop=True)
 
-#Testing with Print Statement:
-print(sampled_data)
-
+random_sample(data_test, "Caseworker58", 2, "sampled_test")
 # #Random Sampling Testing
 # for caseworker in CASEWORKERS:
 #     for _ in range(NUM_SAMPLES):
